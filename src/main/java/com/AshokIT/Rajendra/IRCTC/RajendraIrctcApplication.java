a9fe90db666567d6985ecc93a089574c52b5fd47
Rajendrakumar p <rajendrakumarp99@gmail.com>
2025-06-18 09:09:04 +0530

    first commit

---
+++ src/main/java/com/AshokIT/Rajendra/IRCTC/RajendraIrctcApplication.java
@@ -0,0 +1,13 @@
+package com.AshokIT.Rajendra.IRCTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajendraIrctcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajendraIrctcApplication.class, args);
+	}
+
+}
